e015b2294a8bacf418cac1de8fc7ec25a72474e3
zuhair <zuhair2025@gmail.com>
2020-01-15 15:55:49 +0600

    update app folder

--- app/Http/Controllers/Front/FrontProductController.php
+++ app/Http/Controllers/Front/FrontProductController.php
@@ -2,13 +2,47 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Cart;
 use Illuminate\Http\Request;
+use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Support\Facades\Session;
 
 class FrontProductController extends Controller
 {
     public function index()
     {
-        return view('frontend.product');
+        $product = Product::find( );
+        return view('frontend.product',compact('product'));
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return view('frontend.product',compact('product'));
+    }
+
+    public function getAddToCart(Request $request,$id)
+    {
+        $product = Product::find($id);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product,$product->id);
+
+        $request->session()->put('cart',$cart);
+        // dd($request->session()->get('cart'));
+        return redirect('product/'.$product->id);
+    }
+
+    public function getCart()
+    {
+        if (!Session::has('cart'))
+        {
+            return view('frontend.cart');
+        }
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        return view('frontend.cart',['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
     }
 }
